8fb467441b5ccf20be18c49ce6add52812f6a19c
IacobSon <dorian.iacobescu@gmail.com>
2022-12-23 20:36:08 +0100

    New Elixir project

---
+++ lib/ecspanse.ex
@@ -0,0 +1,18 @@
+defmodule Ecspanse do
+  @moduledoc """
+  Documentation for `Ecspanse`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ecspanse.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecspanse_test.exs
@@ -0,0 +1,8 @@
+defmodule EcspanseTest do
+  use ExUnit.Case
+  doctest Ecspanse
+
+  test "greets the world" do
+    assert Ecspanse.hello() == :world
+  end
+end
