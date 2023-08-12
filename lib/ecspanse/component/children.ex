a285948841a7f65f1d413efe619508f45bd1c6d2
IacobSon <dorian.iacobescu@gmail.com>
2023-08-12 16:45:37 +0200

    Relationships docs

--- lib/ecspanse/component/children.ex
+++ lib/ecspanse/component/children.ex
@@ -1,13 +1,21 @@
 defmodule Ecspanse.Component.Children do
   @moduledoc """
-  A special component provided by the framework to store the Entity's Children.
-  Should use deticated functions and options to modify this component.
+  The `Children` component is a special component provided by the framework
+  to maintain references to an entity's child entities.
 
-  An empty Children component is automatically added when creating entities, even if no childrens are defined at cration time.
+  Dedicated queries and commands are provided to interact with this component.
+
+  An empty `Children` component is automatically added upon entity creation,
+  even if no child entities are defined at the time of creation.
+
+  > #### Ecspanse parent-child relationships are **bidirectional associations** {: .info}
   """
   use Ecspanse.Component,
     state: [entities: []]
 
+  @typedoc """
+  Entity's children list.
+  """
   @type t :: %__MODULE__{
           entities: list(Ecspanse.Entity.t())
         }
--- lib/ecspanse/component/parents.ex
+++ lib/ecspanse/component/parents.ex
@@ -1,13 +1,21 @@
 defmodule Ecspanse.Component.Parents do
   @moduledoc """
-  A special component provided by the framework to store the Entity's Parents.
-  Should use deticated functions and options to modify this component.
+  The `Parents` component is a special component provided by the framework
+  to maintain references to an entity's parent entities.
 
-  An empty Parents component is automatically added when creating entities, event if no parents are defined at cration time.
+  Dedicated queries and commands are provided to interact with this component.
+
+  An empty `Parents` component is automatically added upon entity creation,
+  even if no parent entities are defined at the time of creation.
+
+  > #### Ecspanse parent-child relationships are **bidirectional associations** {: .info}
   """
   use Ecspanse.Component,
     state: [entities: []]
 
+  @typedoc """
+  Entity's parents list.
+  """
   @type t :: %__MODULE__{
           entities: list(Ecspanse.Entity.t())
         }
