58698e4b42b64bda69f9eda39ea75b4310693758
iacobson <dorian.iacobescu@gmail.com>
2024-04-01 13:07:22 +0200

    StateTransition event docs

--- lib/ecspanse/event/state_transition.ex
+++ lib/ecspanse/event/state_transition.ex
@@ -1,6 +1,16 @@
 defmodule Ecspanse.Event.StateTransition do
   @moduledoc """
   Special library event emitted upon a state change.
+
+  ## Examples
+
+    ```elixir
+    %Ecspanse.Event.StateTransition{
+      module: Demo.States.Game,
+      previous_state: :running,
+      current_state: :paused
+    }
+    ```
   """
 
   use Ecspanse.Event, fields: [:module, :previous_state, :current_state]
